86bbe51b052cfa4ee7f210c3c8350461eff5d4a1
Gabriel Nützi (UBUNTU) <gnuetzi@gmx.ch>
2015-07-02 13:35:35 +0200

    added a no-built step in finding meta when meta is downloaded as external_project.

--- cmake/FindMeta.cmake
+++ cmake/FindMeta.cmake
@@ -6,8 +6,10 @@
 
 find_package(PkgConfig)
 
+include(FindPackageHandleStandardArgs)
 
 if(NOT EXISTS "${Meta_INCLUDE_DIR}")
+    message(STATUS "Meta: find meta")
   find_path(Meta_INCLUDE_DIR
     NAMES meta/meta.hpp 
     DOC "Meta library header files"
@@ -17,6 +19,7 @@ endif()
 if(EXISTS "${Meta_INCLUDE_DIR}")
 
 else()
+
   message(STATUS "Meta: Setup External Projext")
   include(ExternalProject)
   ExternalProject_Add(meta
@@ -24,15 +27,16 @@ else()
     TIMEOUT 10
     CMAKE_ARGS -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER} -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
     PREFIX "${CMAKE_CURRENT_BINARY_DIR}"
+    BUILD_COMMAND "" # disable build step
     INSTALL_COMMAND "" # Disable install step
     )
   
   # Specify include dir
   ExternalProject_Get_Property(meta source_dir)
-  set(Meta_INCLUDE_DIR ${source_dir}/include)
+  set(Meta_INCLUDE_DIR ${source_dir}/include CACHE STRING "Meta include directory" FORCE)
+  
 endif()
 
-
-include(FindPackageHandleStandardArgs)
+message(STATUS ${Meta_INCLUDE_DIR})
 find_package_handle_standard_args(Meta DEFAULT_MSG Meta_INCLUDE_DIR)
 mark_as_advanced(Meta_INCLUDE_DIR)
