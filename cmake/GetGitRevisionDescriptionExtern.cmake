42bcb15743cb33d655f8b6b589973d28c497ce4a
Gabriel Nützi (UBUNTU) <gnuetzi@gmx.ch>
2016-02-03 13:23:13 +0100

    git describe fails?

--- cmake/GetGitRevisionDescriptionExtern.cmake
+++ cmake/GetGitRevisionDescriptionExtern.cmake
@@ -1,4 +1,9 @@
 # this file is executed outside of ApproxMVBB to get the revision describtion
 include(cmake/GetGitRevisionDescription.cmake)
 git_describe(ApproxMVBB_VERSION "--tags" "--abbrev=0")
+
+if( NOT ApproxMVBB_VERSION )
+message(FATAL_ERROR "ApproxMVBB library version could not be determined!, ${ApproxMVBB_VERSION}")
+endif()
+
 message(STATUS "${ApproxMVBB_VERSION}")
