454565858b8566ce3876aeb51a2aeb73d5a34706
Gabriel Nützi (UBUNTU) <gnuetzi@gmx.ch>
2016-02-03 17:18:53 +0100

    travis

--- travis/build.sh
+++ travis/build.sh
@@ -12,15 +12,11 @@ if [ -z "$BUILD_TYPE" ]; then export BUILD_TYPE=Release; fi
 # make ApproxMVBB
 echo "Build ApproxMVBB:"
 cd $CHECKOUT_PATH
-pwd
-  ### init submodules
-  #git submodule init
-  #git submodule update
-  ## only for hig perf. tests
-  ##- cd addtional/tests/files; cat Lucy* | tar xz
-  git fetch -t
-  git --version
-  git describe --tags --abbrev=0
+### init submodules
+#git submodule init
+#git submodule update
+## only for hig perf. tests
+##- cd addtional/tests/files; cat Lucy* | tar xz
 
 if [ ! -d $ROOT_PATH/build ]; then mkdir $ROOT_PATH/build; fi
 cd $ROOT_PATH/build
@@ -33,7 +29,7 @@ cd $ROOT_PATH
 echo "Install and test if ApproxMVBB links:"
 mkdir $ROOT_PATH/buildLibUsage
 cd $ROOT_PATH/buildLibUsage
-INSTALL=$(find $ROOT_PATH/build/install/lib/cmake/ApproxMVBB* -type d)
+INSTALL=$(find $ROOT_PATH/build/install/lib/cmake/* -type d)
 echo "Install dir= $INSTALL"
 cmake $CHECKOUT_PATH/example/libraryUsage -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_CXX_FLAGS="${CXX_FLAGS}" -DCMAKE_EXE_LINKER_FLAGS="${CXX_LINKER_FLAGS}" -DApproxMVBB_DIR=$INSTALL
 make VERBOSE=1
