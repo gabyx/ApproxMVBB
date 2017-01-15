c90aecb4e224bcd18ed7e91368ba97dfe87af404
Gabriel Nützi <gnuetzi@gmail.com>
2017-01-15 22:02:43 +0100

    Update install_osx.sh

--- travis/install_osx.sh
+++ travis/install_osx.sh
@@ -10,7 +10,7 @@ export INSTALL_PREFIX="/usr/local/"
 export PATH=$INSTALL_PREFIX/bin:$PATH
 
 # travis bug: https://github.com/travis-ci/travis-ci/issues/6307
-rvm get head
+rvm get head || true
 
 if [ "$CXX" = "g++" ] ; then 
   brew update || echo "suppress failures in order to ignore warnings"
@@ -47,3 +47,5 @@ chmod +x $CHECKOUT_PATH/travis/install_dep.sh
 
 # "DEPENDECIES COMPLETE ================================================================="
  
+# Workaround for https://github.com/travis-ci/travis-ci/issues/6522
+set +e
