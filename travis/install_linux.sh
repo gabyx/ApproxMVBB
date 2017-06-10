9a4ccb874e55c42fb35a3678a4c5b85398de793f
Gabriel Nützi <gnuetzi@gmail.com>
2017-06-10 13:53:03 +0200

    Create install_linux.sh

--- travis/install_linux.sh
+++ travis/install_linux.sh
@@ -10,11 +10,9 @@ export PATH=$INSTALL_PREFIX/bin:$PATH
 cd $ROOT_PATH
 
 #install a newer cmake since at this time Travis only has version 2.8.7
-wget --no-check-certificate http://www.cmake.org/files/v3.2/cmake-3.2.2-Linux-x86_64.sh
-chmod a+x cmake-3.2.2-Linux-x86_64.sh
-sudo ./cmake-3.2.2-Linux-x86_64.sh --skip-license --prefix=$INSTALL_PREFIX
-
-
+wget --no-check-certificate http://www.cmake.org/files/v3.6/cmake-3.6.2-Linux-x86_64.sh
+chmod a+x cmake-3.6.2-Linux-x86_64.sh
+sudo ./cmake-3.6.2-Linux-x86_64.sh --skip-license --prefix=$INSTALL_PREFIX
 
 if [ "$CXX" = "g++" ]; then export CXX="g++-${GCC_VERSION}" CC="gcc-${GCC_VERSION}"; fi
 if [ "$CXX" = "clang++" ] ; then export CXX="clang++-${CLANG_VERSION}" CC="clang-${CLANG_VERSION}"; fi
