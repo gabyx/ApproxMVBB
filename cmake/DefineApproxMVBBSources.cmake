36e9b466fbfde62ce4e493c98c4346cd85d5545a
Gabriel Nuetzi <gnuetzi@gmail.com>
2017-08-27 14:13:44 +0200

    made  globale variable initPredicates which gets initialized at lib loading...

--- cmake/DefineApproxMVBBSources.cmake
+++ cmake/DefineApproxMVBBSources.cmake
@@ -1,33 +1,41 @@
-MACRO(INCLUDE_ALL_ApproxMVBB_SOURCE 
-      SRC 
-      INC 
-      INCLUDE_DIRS 
+MACRO(INCLUDE_ALL_ApproxMVBB_SOURCE
+      SRC
+      INC
+      INCLUDE_DIRS
       DEPENDING_TARGETS # Input variable names
-      ApproxMVBB_ROOT_DIR 
-      ApproxMVBB_BINARY_DIR)  
+      ApproxMVBB_ROOT_DIR
+      ApproxMVBB_BINARY_DIR)
 
 
     # Add all external sources/headers
-    
-    
+
+
     INCLUDE(${ApproxMVBB_ROOT_DIR}/cmake/DefineApproxMVBBExternalSources.cmake)
-    INCLUDE_DIAMETER_SOURCE(           ApproxMVBB_DIAM_SRC ApproxMVBB_DIAM_INC ApproxMVBB_DIAM_INC_DIRS 
-        ${ApproxMVBB_ROOT_DIR}/external/Diameter ${ApproxMVBB_ROOT_DIR} ${ApproxMVBB_BINARY_DIR})
-    
-    INCLUDE_GEOMETRYPREDICATES_SOURCE( ApproxMVBB_GEOMPRED_SRC ApproxMVBB_GEOMPRED_INC ApproxMVBB_GEOMPRED_INC_DIRS ApproxMVBB_GEOMPRED_TARGETS
-        ${ApproxMVBB_ROOT_DIR}/external/GeometryPredicates ${ApproxMVBB_ROOT_DIR} ${ApproxMVBB_BINARY_DIR})
-        
-        
-        
-    
+    INCLUDE_DIAMETER_SOURCE(ApproxMVBB_DIAM_SRC
+                            ApproxMVBB_DIAM_INC
+                            ApproxMVBB_DIAM_INC_DIRS
+                            ${ApproxMVBB_ROOT_DIR}/external/Diameter
+                            ${ApproxMVBB_ROOT_DIR}
+                            ${ApproxMVBB_BINARY_DIR})
+
+    INCLUDE_GEOMETRYPREDICATES_SOURCE( ApproxMVBB_GEOMPRED_SRC
+                                       ApproxMVBB_GEOMPRED_INC
+                                       ApproxMVBB_GEOMPRED_INC_DIRS
+                                       ${ApproxMVBB_ROOT_DIR}/external/GeometryPredicates
+                                       ${ApproxMVBB_ROOT_DIR}
+                                       ${ApproxMVBB_BINARY_DIR})
+
+
+
+
     SET(${SRC}
         ${ApproxMVBB_ROOT_DIR}/src/ApproxMVBB/Common/MyMatrixTypeDefs.cpp
-        ${ApproxMVBB_ROOT_DIR}/src/ApproxMVBB/RandomGenerators.cpp  
-        ${ApproxMVBB_ROOT_DIR}/src/ApproxMVBB/ConvexHull2D.cpp           
-        ${ApproxMVBB_ROOT_DIR}/src/ApproxMVBB/MinAreaRectangle.cpp  
-        ${ApproxMVBB_ROOT_DIR}/src/ApproxMVBB/OOBB.cpp              
-        ${ApproxMVBB_ROOT_DIR}/src/ApproxMVBB/AABB.cpp   
-        
+        ${ApproxMVBB_ROOT_DIR}/src/ApproxMVBB/RandomGenerators.cpp
+        ${ApproxMVBB_ROOT_DIR}/src/ApproxMVBB/ConvexHull2D.cpp
+        ${ApproxMVBB_ROOT_DIR}/src/ApproxMVBB/MinAreaRectangle.cpp
+        ${ApproxMVBB_ROOT_DIR}/src/ApproxMVBB/OOBB.cpp
+        ${ApproxMVBB_ROOT_DIR}/src/ApproxMVBB/AABB.cpp
+
         ${ApproxMVBB_DIAM_SRC}
         ${ApproxMVBB_GEOMPRED_SRC}
         ${ApproxMVBB_META_SRC}
@@ -41,23 +49,23 @@ MACRO(INCLUDE_ALL_ApproxMVBB_SOURCE
         ${ApproxMVBB_ROOT_DIR}/include/ApproxMVBB/Common/StaticAssert.hpp
         ${ApproxMVBB_ROOT_DIR}/include/ApproxMVBB/Common/AssertionDebug.hpp
         ${ApproxMVBB_ROOT_DIR}/include/ApproxMVBB/Common/Platform.hpp
-        
-        
-        ${ApproxMVBB_ROOT_DIR}/include/ApproxMVBB/AABB.hpp  
-        ${ApproxMVBB_ROOT_DIR}/include/ApproxMVBB/AngleFunctions.hpp  
-        ${ApproxMVBB_ROOT_DIR}/include/ApproxMVBB/ComputeApproxMVBB.hpp 
-        ${ApproxMVBB_ROOT_DIR}/include/ApproxMVBB/ContainerFunctions.hpp         
-        ${ApproxMVBB_ROOT_DIR}/include/ApproxMVBB/ConvexHull2D.hpp       
-        ${ApproxMVBB_ROOT_DIR}/include/ApproxMVBB/GreatestCommonDivisor.hpp  
-        ${ApproxMVBB_ROOT_DIR}/include/ApproxMVBB/MakeCoordinateSystem.hpp     
-        ${ApproxMVBB_ROOT_DIR}/include/ApproxMVBB/MinAreaRectangle.hpp  
-        ${ApproxMVBB_ROOT_DIR}/include/ApproxMVBB/OOBB.hpp  
-        ${ApproxMVBB_ROOT_DIR}/include/ApproxMVBB/PointFunctions.hpp     
+
+
+        ${ApproxMVBB_ROOT_DIR}/include/ApproxMVBB/AABB.hpp
+        ${ApproxMVBB_ROOT_DIR}/include/ApproxMVBB/AngleFunctions.hpp
+        ${ApproxMVBB_ROOT_DIR}/include/ApproxMVBB/ComputeApproxMVBB.hpp
+        ${ApproxMVBB_ROOT_DIR}/include/ApproxMVBB/ContainerFunctions.hpp
+        ${ApproxMVBB_ROOT_DIR}/include/ApproxMVBB/ConvexHull2D.hpp
+        ${ApproxMVBB_ROOT_DIR}/include/ApproxMVBB/GreatestCommonDivisor.hpp
+        ${ApproxMVBB_ROOT_DIR}/include/ApproxMVBB/MakeCoordinateSystem.hpp
+        ${ApproxMVBB_ROOT_DIR}/include/ApproxMVBB/MinAreaRectangle.hpp
+        ${ApproxMVBB_ROOT_DIR}/include/ApproxMVBB/OOBB.hpp
+        ${ApproxMVBB_ROOT_DIR}/include/ApproxMVBB/PointFunctions.hpp
         ${ApproxMVBB_ROOT_DIR}/include/ApproxMVBB/ProjectedPointSet.hpp
-        ${ApproxMVBB_ROOT_DIR}/include/ApproxMVBB/KdTree.hpp  
+        ${ApproxMVBB_ROOT_DIR}/include/ApproxMVBB/KdTree.hpp
         ${ApproxMVBB_ROOT_DIR}/include/ApproxMVBB/KdTreeXml.hpp
         ${ApproxMVBB_ROOT_DIR}/include/ApproxMVBB/TypeDefsPoints.hpp
-        
+
         ${ApproxMVBB_DIAM_INC}
         ${ApproxMVBB_GEOMPRED_INC}
         ${ApproxMVBB_META_INC}
@@ -65,24 +73,25 @@ MACRO(INCLUDE_ALL_ApproxMVBB_SOURCE
     )
 
     SET(${INCLUDE_DIRS}
-        ${ApproxMVBB_GEOMPRED_INC_DIRS} 
+        ${ApproxMVBB_GEOMPRED_INC_DIRS}
         ${ApproxMVBB_DIAM_INC_DIRS}
         ${ApproxMVBB_META_INC_DIRS}
         ${ApproxMVBB_PUGIXML_INC_DIRS}
         ${ApproxMVBB_ROOT_DIR}/include
         ${ApproxMVBB_BINARY_DIR}/include
     )
-    
-    SET(${DEPENDING_TARGETS} ${ApproxMVBB_GEOMPRED_TARGETS})
+
+    # No depending targets
+    SET(${DEPENDING_TARGETS} "")
 
     # WRITE CONFIGURATION FILE
-    
+
     INCLUDE(${ApproxMVBB_ROOT_DIR}/cmake/WriteConfigFile.cmake)
     SET(ApproxMVBB_CONFIG_FILE ${ApproxMVBB_BINARY_DIR}/include/ApproxMVBB/Config/Config.hpp)
     MESSAGE(STATUS "ApproxMVBB: Write config file ${ApproxMVBB_CONFIG_FILE}, ${ApproxMVBB_OPENMP_NTHREADS}")
     ApproxMVBB_WRITE_CONFIG_FILE( ${ApproxMVBB_CONFIG_FILE} ${ApproxMVBB_ROOT_DIR})
     #=========================
-    
-    
-    
+
+
+
 ENDMACRO()
--- external/GeometryPredicates/src/PredicatesInit.cpp
+++ external/GeometryPredicates/src/PredicatesInit.cpp
@@ -7,94 +7,81 @@
 //  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 // ========================================================================================
 
-#include <stdio.h>
-
+#include "ApproxMVBB/GeometryPredicates/PredicatesInit.hpp"
 /* FPU control. We MUST have only double precision (not extended precision) */
 #include "ApproxMVBB/GeometryPredicates/Rounding.hpp"
 
-int main(int, char* [])
+namespace GeometryPredicates
 {
-    FPU_DECLARE
+  //! Initilaize the variable predicatesInit
+  PredicatesInit predicatesInit ;
 
-    double half     = 0.5;
-    double check    = 1.0, lastcheck;
-    int every_other = 1;
-    /* epsilon = 2^(-p).  Used to estimate roundoff errors. */
-    double epsilon = 1.0;
-    /* splitter = 2^ceiling(p / 2) + 1.  Used to split floats in half. */
-    double splitter = 1.0;
-    /* A set of coefficients used to calculate maximum roundoff errors. */
-    double resulterrbound;
-    double ccwerrboundA, ccwerrboundB, ccwerrboundC;
-    double o3derrboundA, o3derrboundB, o3derrboundC;
-    double iccerrboundA, iccerrboundB, iccerrboundC;
-    double isperrboundA, isperrboundB, isperrboundC;
+  //! Constructor which computes the essential values for the predicates.
+  PredicatesInit::PredicatesInit()
+  {
+      // std::cout << "PredicatesInit::CTOR called!!" << std::endl;
+      FPU_DECLARE
 
-    FPU_ROUND_DOUBLE;
+      double half     = 0.5;
+      double check    = 1.0, lastcheck;
+      int every_other = 1;
+      /* epsilon = 2^(-p).  Used to estimate roundoff errors. */
+      double epsilon = 1.0;
+      /* splitter = 2^ceiling(p / 2) + 1.  Used to split floats in half. */
+      splitter = 1.0;
 
-    epsilon  = 1.0;
-    splitter = 1.0;
-    /* Repeatedly divide `epsilon' by two until it is too small to add to   */
-    /* one without causing roundoff.  (Also check if the sum is equal to    */
-    /* the previous sum, for machines that round up instead of using exact  */
-    /* rounding.  Not that this library will work on such machines anyway). */
-    do
-    {
-        lastcheck = check;
-        epsilon *= half;
-        if (every_other)
-        {
-            splitter *= 2.0;
-        }
-        every_other = !every_other;
-        check       = 1.0 + epsilon;
-    } while ((check != 1.0) && (check != lastcheck));
-    splitter += 1.0;
-    /* Error bounds for orientation and incircle tests. */
-    resulterrbound = (3.0 + 8.0 * epsilon) * epsilon;
-    ccwerrboundA   = (3.0 + 16.0 * epsilon) * epsilon;
-    ccwerrboundB   = (2.0 + 12.0 * epsilon) * epsilon;
-    ccwerrboundC   = (9.0 + 64.0 * epsilon) * epsilon * epsilon;
-    o3derrboundA   = (7.0 + 56.0 * epsilon) * epsilon;
-    o3derrboundB   = (3.0 + 28.0 * epsilon) * epsilon;
-    o3derrboundC   = (26.0 + 288.0 * epsilon) * epsilon * epsilon;
-    iccerrboundA   = (10.0 + 96.0 * epsilon) * epsilon;
-    iccerrboundB   = (4.0 + 48.0 * epsilon) * epsilon;
-    iccerrboundC   = (44.0 + 576.0 * epsilon) * epsilon * epsilon;
-    isperrboundA   = (16.0 + 224.0 * epsilon) * epsilon;
-    isperrboundB   = (5.0 + 72.0 * epsilon) * epsilon;
-    isperrboundC   = (71.0 + 1408.0 * epsilon) * epsilon * epsilon;
+      FPU_ROUND_DOUBLE;
 
-    FILE* pFile = fopen("PredicatesInit.hpp", "w");
+      epsilon  = 1.0;
+      splitter = 1.0;
+      /* Repeatedly divide `epsilon' by two until it is too small to add to   */
+      /* one without causing roundoff.  (Also check if the sum is equal to    */
+      /* the previous sum, for machines that round up instead of using exact  */
+      /* rounding.  Not that this library will work on such machines anyway). */
+      do
+      {
+          lastcheck = check;
+          epsilon *= half;
+          if (every_other)
+          {
+              splitter *= 2.0;
+          }
+          every_other = !every_other;
+          check       = 1.0 + epsilon;
+      } while ((check != 1.0) && (check != lastcheck));
+      splitter += 1.0;
+      /* Error bounds for orientation and incircle tests. */
+      resulterrbound = (3.0 + 8.0 * epsilon) * epsilon;
+      ccwerrboundA   = (3.0 + 16.0 * epsilon) * epsilon;
+      ccwerrboundB   = (2.0 + 12.0 * epsilon) * epsilon;
+      ccwerrboundC   = (9.0 + 64.0 * epsilon) * epsilon * epsilon;
+      o3derrboundA   = (7.0 + 56.0 * epsilon) * epsilon;
+      o3derrboundB   = (3.0 + 28.0 * epsilon) * epsilon;
+      o3derrboundC   = (26.0 + 288.0 * epsilon) * epsilon * epsilon;
+      iccerrboundA   = (10.0 + 96.0 * epsilon) * epsilon;
+      iccerrboundB   = (4.0 + 48.0 * epsilon) * epsilon;
+      iccerrboundC   = (44.0 + 576.0 * epsilon) * epsilon * epsilon;
+      isperrboundA   = (16.0 + 224.0 * epsilon) * epsilon;
+      isperrboundB   = (5.0 + 72.0 * epsilon) * epsilon;
+      isperrboundC   = (71.0 + 1408.0 * epsilon) * epsilon * epsilon;
 
-    fputs(
-        "/* This file was generated automatically by PredicatsInit.c */\n"
-        "// ========================================================================================\n"
-        "//  ApproxMVBB\n"
-        "//  Copyright (C) 2014 by Gabriel Nützi <nuetzig (at) imes (d0t) mavt (d0t) ethz (d0t) ch>\n"
-        "//\n"
-        "//  This Source Code Form is subject to the terms of the Mozilla Public\n"
-        "//  License, v. 2.0. If a copy of the MPL was not distributed with this\n"
-        "//  file, You can obtain one at http://mozilla.org/MPL/2.0/.\n"
-        "// ========================================================================================\n",
-        pFile);
-    fprintf(pFile, "static double splitter = %f;\n", splitter);
-    fprintf(pFile, "static double resulterrbound = %.16g;\n", resulterrbound);
-    fprintf(pFile, "static double ccwerrboundA = %.16g;\n", ccwerrboundA);
-    fprintf(pFile, "static double ccwerrboundB = %.16g;\n", ccwerrboundB);
-    fprintf(pFile, "static double ccwerrboundC = %.16g;\n", ccwerrboundC);
-    fprintf(pFile, "static double o3derrboundA = %.16g;\n", o3derrboundA);
-    fprintf(pFile, "static double o3derrboundB = %.16g;\n", o3derrboundB);
-    fprintf(pFile, "static double o3derrboundC = %.16g;\n", o3derrboundC);
-    fprintf(pFile, "static double iccerrboundA = %.16g;\n", iccerrboundA);
-    fprintf(pFile, "static double iccerrboundB = %.16g;\n", iccerrboundB);
-    fprintf(pFile, "static double iccerrboundC = %.16g;\n", iccerrboundC);
-    fprintf(pFile, "static double isperrboundA = %.16g;\n", isperrboundA);
-    fprintf(pFile, "static double isperrboundB = %.16g;\n", isperrboundB);
-    fprintf(pFile, "static double isperrboundC = %.16g;\n", isperrboundC);
-    fclose(pFile);
 
-    FPU_RESTORE;
+      // printf("splitter = %f;\n", splitter);
+      // printf("resulterrbound = %.16g;\n", resulterrbound);
+      // printf("ccwerrboundA = %.16g;\n", ccwerrboundA);
+      // printf("ccwerrboundB = %.16g;\n", ccwerrboundB);
+      // printf("ccwerrboundC = %.16g;\n", ccwerrboundC);
+      // printf("o3derrboundA = %.16g;\n", o3derrboundA);
+      // printf("o3derrboundB = %.16g;\n", o3derrboundB);
+      // printf("o3derrboundC = %.16g;\n", o3derrboundC);
+      // printf("iccerrboundA = %.16g;\n", iccerrboundA);
+      // printf("iccerrboundB = %.16g;\n", iccerrboundB);
+      // printf("iccerrboundC = %.16g;\n", iccerrboundC);
+      // printf("isperrboundA = %.16g;\n", isperrboundA);
+      // printf("isperrboundB = %.16g;\n", isperrboundB);
+      // printf("isperrboundC = %.16g;\n", isperrboundC);
 
-    return 0;
+      FPU_RESTORE;
+  }
+
 }
