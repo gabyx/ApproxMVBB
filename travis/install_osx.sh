34b5aa888e9e09a95e8c54bca0bb1e20d3d7c95d
Gabriel Nuetzi <gnuetzi@gmail.com>
2017-12-27 02:21:54 +0100

    fixTravis

--- travis/install_osx.sh
+++ travis/install_osx.sh
@@ -16,16 +16,15 @@ export PATH="$PATH:$INSTALL_PREFIX/bin"
 brew update || echo "suppress failures in order to ignore warnings"
 brew tap homebrew/versions || echo "suppress failures in order to ignore warnings"
 
-
-
 # eigen3 needs gfortran
 brew install gcc || echo "suppress failures in order to ignore warnings"
 brew link --overwrite --dry-run gcc
 brew link --overwrite gcc
 
-brew install llvm
+brew install llvm || echo "suppress failures in order to ignore warnings"
 brew link --overwrite --dry-run llvm
 brew link --overwrite llvm
+export PATH="/usr/local/opt/llvm/bin:$PATH"
 
 # Cmake
 brew install cmake || echo "suppress failures in order to ignore warnings"
