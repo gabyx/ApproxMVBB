450b5321fafdcb99d99031c32c95df3686a1d63f
Gabriel Nuetzi <gnuetzi@gmail.com>
2017-12-27 01:33:20 +0100

    travis build os x fixing

--- travis/install_osx.sh
+++ travis/install_osx.sh
@@ -18,6 +18,7 @@ brew tap homebrew/versions || echo "suppress failures in order to ignore warning
 
 # eigen3 needs gfortran
 brew install gcc || echo "suppress failures in order to ignore warnings"
+brew install gfortran || echo "suppress failures in order to ignore warnings"
 
 # Cmake
 brew install cmake || echo "suppress failures in order to ignore warnings"
