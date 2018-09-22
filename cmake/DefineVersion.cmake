e3375fbb81f877405c95d59152c327dc92b05688
Gabriel Nuetzi <gnuetzi@gmail.com>
2018-09-22 20:01:55 +0200

    Buildcorrections: if no .git folder extract version from Versionfile (ZIP).

--- cmake/DefineVersion.cmake
+++ cmake/DefineVersion.cmake
@@ -1,14 +1,28 @@
 macro(defineVersion ApproxMVBB_ROOT_DIR)
     # Get the version of the project ApproxMVBB
-    execute_process(COMMAND "cmake" "-P" "cmake/GetGitRevisionDescriptionExtern.cmake" 
-        WORKING_DIRECTORY "${ApproxMVBB_ROOT_DIR}"
-        OUTPUT_VARIABLE ApproxMVBB_VERSION ERROR_VARIABLE Error
-    )
+    # if there is a git directory otherwise take it from 
+    # the version file
+    if(EXISTS "${ApproxMVBB_ROOT_DIR}/.git")
+        execute_process(COMMAND "cmake" "-P" "cmake/GetGitRevisionDescriptionExtern.cmake" 
+            WORKING_DIRECTORY "${ApproxMVBB_ROOT_DIR}"
+            OUTPUT_VARIABLE ApproxMVBB_VERSION ERROR_VARIABLE Error
+        )
+        MESSAGE(STATUS "ApproxMVBB Version: ${ApproxMVBB_VERSION} extracted from git tags!")
+    else()
+        file(STRINGS "${ApproxMVBB_ROOT_DIR}/VERSION" ApproxMVBB_VERSION LIMIT_COUNT 1)
+        MESSAGE(STATUS "ApproxMVBB Version: ${ApproxMVBB_VERSION} extracted from version file!")
+    endif()
+
 
     if(Error)
         message(FATAL_ERROR "Error in getting version of ApproxMVBB ${Error}" FATAL)
     endif()
 
-    message(STATUS "ApproxMVBB Version: ${ApproxMVBB_VERSION_STRING} extracted from git tags!")
+    string(REGEX REPLACE "^.*v([0-9]+)\\..*" "\\1" ApproxMVBB_VERSION_MAJOR "${ApproxMVBB_VERSION}")
+    string(REGEX REPLACE "^.*v[0-9]+\\.([0-9]+).*" "\\1" ApproxMVBB_VERSION_MINOR "${ApproxMVBB_VERSION}")
+    string(REGEX REPLACE "^.*v[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" ApproxMVBB_VERSION_PATCH "${ApproxMVBB_VERSION}")
+    string(REGEX REPLACE "^.*v[0-9]+\\.[0-9]+\\.[0-9]+(.*)" "\\1" ApproxMVBB_VERSION_SHA1 "${ApproxMVBB_VERSION}")
+    set(ApproxMVBB_VERSION_STRING "${ApproxMVBB_VERSION_MAJOR}.${ApproxMVBB_VERSION_MINOR}.${ApproxMVBB_VERSION_PATCH}")
+    
 endmacro()
 
--- cmake/WriteConfigFile.cmake
+++ cmake/WriteConfigFile.cmake
@@ -1,22 +1,7 @@
+include(DefineVersion)
 MACRO(ApproxMVBB_WRITE_CONFIG_FILE ApproxMVBB_CONFIG_FILE ApproxMVBB_ROOT_DIR )
 
-    # Get the version of the project ApproxMVBB
-    execute_process(COMMAND "cmake" "-P" "cmake/GetGitRevisionDescriptionExtern.cmake" 
-        WORKING_DIRECTORY "${ApproxMVBB_ROOT_DIR}"
-        OUTPUT_VARIABLE ApproxMVBB_VERSION ERROR_VARIABLE Error
-    )
-
-    if(Error)
-        message(FATAL_ERROR "Error in getting version of ApproxMVBB ${Error}" FATAL)
-    endif()
-
-
-    string(REGEX REPLACE "^.*v([0-9]+)\\..*" "\\1" ApproxMVBB_VERSION_MAJOR "${ApproxMVBB_VERSION}")
-    string(REGEX REPLACE "^.*v[0-9]+\\.([0-9]+).*" "\\1" ApproxMVBB_VERSION_MINOR "${ApproxMVBB_VERSION}")
-    string(REGEX REPLACE "^.*v[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" ApproxMVBB_VERSION_PATCH "${ApproxMVBB_VERSION}")
-    string(REGEX REPLACE "^.*v[0-9]+\\.[0-9]+\\.[0-9]+(.*)" "\\1" ApproxMVBB_VERSION_SHA1 "${ApproxMVBB_VERSION}")
-    set(ApproxMVBB_VERSION_STRING "${ApproxMVBB_VERSION_MAJOR}.${ApproxMVBB_VERSION_MINOR}.${ApproxMVBB_VERSION_PATCH}")
-    MESSAGE(STATUS "ApproxMVBB Version: ${ApproxMVBB_VERSION_STRING} extracted from git tags!")
+    defineVersion(${ApproxMVBB_ROOT_DIR})
 
     configure_file(
       ${ApproxMVBB_ROOT_DIR}/include/ApproxMVBB/Config/Config.hpp.in.cmake
